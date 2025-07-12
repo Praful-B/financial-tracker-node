b69238edefe34e25674b039a408eaadb2936d7ee
Praful-B <baleraoprafulchandra2006@gmail.com>
2025-07-13 02:16:41 +0530

    solid fallback

--- app.js
+++ app.js
@@ -2,29 +2,20 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-/*
-  The routes go here example below:
-  const a_home_route = require('./routes/a_home');
-  const a_newArticle_route = require('./routes/a_newArticle');
-  const a_editArticle_route = require('./routes/a_editArticle');
-
-  const g_home_route = require('./routes/g_home');
-  const g_article_route = require('./routes/g_article');
-*/
+const authentication_route = require("./routes/authentication");
 
 app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// const routes = require('./routes/')
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use('/home', a_home_route);
+app.use("/auth", authentication_route);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.redirect("/auth");
 });
 
 app.listen(process.env.PORT, () => {
